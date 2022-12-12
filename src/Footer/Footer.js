4b59da7b1c4bfd7ede8e35aa80da7a1dac657eee
SerhiiChynin <sergey.chinin@gmail.com>
2022-12-12 20:25:26 +0200

    project_react

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="Footer-site-footer">
+            <b>React site</b>
+            <p>2022 copyright. All right reserved</p>
+        </div>
+    )
+}
+export default Footer;
---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="Header-site-header">
+            <h1>React site</h1>
+            <h1>utp slogan</h1>
+        </div>
+    );
+}
+
+export default Header;
